7c0270eb4ccb16286d0cdc720f8c71fdd3ec4120
gobardhanm <gobardhan.meh3r@gmail.com>
2026-03-15 20:27:14 +0530

    process module

---
+++ nodeJs-traversyMedia/processDemo.js
@@ -0,0 +1,14 @@
+// argv
+console.log(process.argv);
+console.log(process.argv[2]);
+
+// process.env
+console.log(process.env.LOGNAME);
+
+// pid
+console.log(process.pid);
+
+// cwd()
+console.log(process.cwd());
+
+// etc.
