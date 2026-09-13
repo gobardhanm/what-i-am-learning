01ed706c212b9bbc8efcdb3f77bb9afbdfd3dba5
gobardhanm <gobardhan.meh3r@gmail.com>
2026-09-13 11:04:17 +0530

    Started with Java FS FDE - Java

---
+++ Java-FS-FDE/Class_3/Collections/src/App.java
@@ -0,0 +1,61 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+/*
+Collections:
+
+- It is a container that groups elements into single unit
+
+all elements inside collections should be objects
+
+it is used to retrieve manipulate group of data
+
+java.util
+
+Ex:
+- List of bank account
+- List of Employees
+- Set of students
+- Group of mobile number
+
+Advantages:
+
+- Increase program speed and quality.
+- Grows dynamically
+
+Collections:
+Size: Grows Dynamically
+Objects/Non-primitive
+
+
+{ But in Arrays - fixed size 
+ - primitive/ non-primitive;
+ Arrays are faster than collection }
+
+- Memory: Collections are recommended over arrays;
+- Collection has many utility methods
+
+
+Collection Hierarchy:
+
+                    Collection
+                        |
+    |                   |           |           |
+    List                Set         Queue       Map
+                        |                       |
+                        SortedSet               SortedMap
+
+
+- Collection is an Interface.
+- It is root interface in collection hierarchy.
+
+- An interface can't extend a class in Java.
+- Interface extends Interface --> Possible.
+- Interface can't 
+
+- 
+ */
---
+++ Java-FS-FDE/Class_3/src/CheckedExceptionTryWithResources.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class CheckedExceptionTryWithResources {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
